7d04f3b8b01522aacf6002e0ab60a16eb615e3d1
sahev <s4mus.sh@gmail.com>
2020-03-19 11:23:47 -0300

    calcframwork472

---
+++ xunitdemo/calc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace xunitdemo
+{
+    public class Calculator
+    {
+
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Fact()
+        {
+             Calculator calc = new Calculator();
+
+            Assert.Equal(4, calc.Add(2, 2));
+        }
+
+        [Theory]
+        [InlineData(5, 2, 3)]
+        [InlineData(10, 5, 5)]
+        [InlineData(8, 1, 0)]
+        public void Theory(int expected, int numA, int numB)
+        {
+            Calculator calc = new Calculator();
+
+            Assert.Equal(expected, calc.Add(numA, numB));
+        }
+
+
+    }
+}
